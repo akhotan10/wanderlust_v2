29d3801e1e6ed3aed18b3ec381e47f56dfd44119
anonymous anonymous <akhotan10@gmail.com>
2021-12-02 16:46:55 +0000

    Generate graphiti resources for dinings

---
+++ app/resources/dining_resource.rb
@@ -0,0 +1,15 @@
+class DiningResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :dining_type, :string_enum, allow: Dining.dining_types.keys
+  attribute :dining_name, :string
+  attribute :rating, :integer
+  attribute :location_id, :integer
+  attribute :trip_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
