fa43ff60be3b4313c0718ced5f1ab45df6ab5a92
anonymous anonymous <akhotan10@gmail.com>
2021-12-02 16:45:40 +0000

    Validate presence of trips.end_date

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,6 +5,8 @@ class Trip < ApplicationRecord
 
   # Validations
 
+  validates :end_date, :presence => { :message => "Please enter an end date" }
+
   validates :start_date, :presence => { :message => "Please enter a start date" }
 
   validates :title, :presence => { :message => "Please enter a title" }
