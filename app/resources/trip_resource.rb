9bc176953af032d88e7a560fba9e72d5fc251d2a
anonymous anonymous <akhotan10@gmail.com>
2021-12-02 16:46:48 +0000

    Generate graphiti resources for trips

---
+++ app/resources/trip_resource.rb
@@ -0,0 +1,16 @@
+class TripResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :cover_photo, :string
+  attribute :start_date, :date
+  attribute :end_date, :date
+  attribute :photo_album_link, :string
+  attribute :user_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
